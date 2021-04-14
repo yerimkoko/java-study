76e7d318a4246fbc07cee085a3e5695f166cf21a
yerimkoko <gochi97@naver.com>
2021-04-14 18:15:44 +0900

    ch1: overloading

--- src/com/study/ch1/Application.java
+++ src/com/study/ch1/Application.java
@@ -3,9 +3,13 @@ package com.study.ch1;
 public class Application {
 
 	public static void main(String[] args) {
-		Stack stack = new Stack(100);
-		stack.push(5);
-		stack.pop();
+		Stack stack = new Stack();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
 	}
 
 }
