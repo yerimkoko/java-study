b97d9836327295bf9deeaef4f1784c54a120b33d
yerimkoko <gochi97@naver.com>
2021-04-16 18:52:14 +0900

    hw: ch2

--- src/com/study/ch2/Application.java
+++ src/com/study/ch2/Application.java
@@ -3,6 +3,23 @@ package com.study.ch2;
 public class Application {
 
 	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack(10);
+		stack.push(100);
+		stack.push(200);
+		stack.push(300);
+
+		while (!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
+
+		Stack<String> stack2 = new Stack(10);
+		stack2.push("고예리임");
+		stack2.push("강승호오");
+		stack2.push("지짜");
+
+		while (!stack2.isEmpty()) {
+			System.out.println(stack2.pop());
+		}
 
 	}
 
---
+++ src/com/study/ch2/Stack.java
@@ -0,0 +1,32 @@
+package com.study.ch2;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack<T> {
+
+    private int size;
+    private T[] arr;
+    private int top;
+
+    public Stack(int size) {
+        this.size = size;
+        this.top = 0;
+        this.arr = (T[]) new Object[size];
+    }
+
+    public void push(T data) {
+        this.arr[top++] = data;
+    }
+
+    public T pop() {
+        return arr[--top];
+    }
+
+    public boolean isEmpty() {
+        return top == 0;
+    }
+
+
+}
