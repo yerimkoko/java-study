cf704e2a293bba6e365b41a3aee1a54dc1075507
yerimkoko <gochi97@naver.com>
2021-04-28 16:48:22 +0900

    ch5. 수정

--- src/com/study/ch5/ListQueue.java
+++ src/com/study/ch5/ListQueue.java
@@ -13,9 +13,6 @@ public class ListQueue<T> implements Queue<T> {
         arrayList.add(data);
     }
     public T dequeue() {
-        if (arrayList.get(size) == null) {
-            throw new IllegalArgumentException("내보낼 값이 없습니다.");
-        }
         return arrayList.get(size++);
     }
 }
